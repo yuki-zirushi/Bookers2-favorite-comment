e8bffd2a5f6a1a4a2ae4c23c677bba57becad61e
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-02 08:50:06 +0000

    [Update] createアクション

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,9 @@
 class FavoritesController < ApplicationController
   
   def create
+    @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
+    @favorite.save
+    redirect_to("/books")
   end
   
   def destroy
