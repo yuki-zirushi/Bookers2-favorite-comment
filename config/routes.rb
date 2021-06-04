0c7fdaf6fd4fce4b6267aca040eea97825a6bf01
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-04 13:32:13 +0000

    [Add] コメントテーブル　ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  
+
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :index, :edit, :update]
-  
+
 end
