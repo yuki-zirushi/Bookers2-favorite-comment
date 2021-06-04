132245da23672fa213d901b53c0e5d6350758b15
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-04 04:57:15 +0000

    [Add] 非同期化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render "index"
     end
   end
 
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
-  
+
   def create
     @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @favorite.save
-    redirect_to("/books")
   end
-  
+
   def destroy
     @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
     @favorite.destroy
-    redirect_to("/books")
   end
-  
+
 end
