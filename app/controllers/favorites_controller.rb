92192d9d1b9aedfed3a039c4cd8d39f16b7f35a1
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-04 05:47:59 +0000

    [Add]　非同期通信 いいね

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
-
+  before_action :book_params
+  
   def create
     @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @favorite.save
@@ -8,6 +9,12 @@ class FavoritesController < ApplicationController
   def destroy
     @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
     @favorite.destroy
+  end
+  
+  private
+  
+  def book_params
+    @book = Book.find(params[:id])
   end
 
 end
