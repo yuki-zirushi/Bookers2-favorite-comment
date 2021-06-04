395948afda84731b32cf5985839a65620b0c9f43
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-04 12:50:00 +0000

    [Fix] createアクション・destroyアクション

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :book_params
+  before_action :authenticate_user!
   
   def create
-    @favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
-    @favorite.save
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    redirect_to request.referer
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
-    @favorite.destroy
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    redirect_to request.referer
   end
-  
-  private
-  
-  def book_params
-    @book = Book.find(params[:id])
-  end
-
 end
