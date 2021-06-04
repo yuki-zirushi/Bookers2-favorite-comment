5f88fb5b3afa48c25e84cf27b0e46bb6030901d0
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-04 13:23:03 +0000

    [Create] BookCommentモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_085643) do
+ActiveRecord::Schema.define(version: 2021_06_04_132029) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
